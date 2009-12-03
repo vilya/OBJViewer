dd420a9815ce8db948984be4c660d512fccc7bd9
Vilya Harvey <vilya@kamaji.local>
2009-12-03 01:28:52 +0000

    Added some extra reporting to the objstats.py script, to help with debugging.

--- contrib/objstats.py
+++ contrib/objstats.py
@@ -6,13 +6,36 @@ def main():
   low = [10000.0] * 3
   high = [-10000.0] * 3
 
+  faces = 0
+  vertexes = 0
+  texture_vertexes = 0
+  param_vertexes = 0
+  normals = 0
+
   with open(sys.argv[1]) as f:
-    for v in (map(float, line[2:].split()) for line in f if line.startswith('v ')):
-      low = [min(*c) for c in zip(low, v)]
-      high = [max(*c) for c in zip(high, v)]
+    for line in f:
+      if line.startswith('f '):
+        faces += 1
+      elif line.startswith('v '):
+        vertexes += 1
+        v = map(float, line[2:].split())
+        low = [min(*c) for c in zip(low, v)]
+        high = [max(*c) for c in zip(high, v)]
+      elif line.startswith('vt '):
+        texture_vertexes += 1
+      elif line.startswith('vp '):
+        param_vertexes += 1
+      elif line.startswith('vn '):
+        normals += 1
 
   for result in zip( ('x', 'y', 'z'), low, high ):
     print "%s = %f - %f" % tuple(result)
+  print ""
+  print faces, "faces"
+  print vertexes, "vertexes"
+  print texture_vertexes, "texture vertexes"
+  print param_vertexes, "parameter vertexes"
+  print normals, "normals"
 
 
 if __name__ == '__main__':
