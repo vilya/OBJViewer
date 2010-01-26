e1eb2f87e7932411be8783dff6085e635c829ac5
Vilya Harvey <vilya@kamaji.local>
2010-01-26 23:40:11 +0000

    Improved the obstats.py script.

--- contrib/objstats.py
+++ contrib/objstats.py
@@ -3,39 +3,47 @@ import sys
 
 
 def main():
-  low = [10000.0] * 3
-  high = [-10000.0] * 3
+  for model in sys.argv[1:]:
+    low = [10000.0] * 3
+    high = [-10000.0] * 3
 
-  faces = 0
-  vertexes = 0
-  texture_vertexes = 0
-  param_vertexes = 0
-  normals = 0
+    faces = 0
+    vertexes = 0
+    texture_vertexes = 0
+    param_vertexes = 0
+    normals = 0
 
-  with open(sys.argv[1]) as f:
-    for line in f:
-      if line.startswith('f ') or line.startswith('fo '):
-        faces += 1
-      elif line.startswith('v '):
-        vertexes += 1
-        v = map(float, line[2:].split())
-        low = [min(*c) for c in zip(low, v)]
-        high = [max(*c) for c in zip(high, v)]
-      elif line.startswith('vt '):
-        texture_vertexes += 1
-      elif line.startswith('vp '):
-        param_vertexes += 1
-      elif line.startswith('vn '):
-        normals += 1
+    biggest_face = 0
 
-  for result in zip( ('x', 'y', 'z'), low, high ):
-    print "%s = %f - %f" % tuple(result)
-  print ""
-  print faces, "faces"
-  print vertexes, "vertexes"
-  print texture_vertexes, "texture vertexes"
-  print param_vertexes, "parameter vertexes"
-  print normals, "normals"
+    print "--"
+    print model
+    with open(sys.argv[1]) as f:
+      for line in f:
+        if line.startswith('f ') or line.startswith('fo '):
+          faces += 1
+          face_size = len(line.split()) - 1
+          if face_size > biggest_face:
+            biggest_face = face_size
+        elif line.startswith('v '):
+          vertexes += 1
+          v = map(float, line[2:].split())
+          low = [min(*c) for c in zip(low, v)]
+          high = [max(*c) for c in zip(high, v)]
+        elif line.startswith('vt '):
+          texture_vertexes += 1
+        elif line.startswith('vp '):
+          param_vertexes += 1
+        elif line.startswith('vn '):
+          normals += 1
+
+    for result in zip( ('x', 'y', 'z'), low, high ):
+      print "%s = %f - %f" % tuple(result)
+    print ""
+    print "%d faces of up to %d vertices" % (faces, biggest_face)
+    print vertexes, "vertexes"
+    print texture_vertexes, "texture vertexes"
+    print param_vertexes, "parameter vertexes"
+    print normals, "normals"
 
 
 if __name__ == '__main__':
