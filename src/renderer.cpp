4ad849128a02c08d32cde5ff8c290f58725b55b2
Vilya Harvey <vilya.harvey@gmail.com>
2010-01-27 19:30:29 +0000

    Switched the default texture mode to repeat instead of clamp.
    Models generally look a lot better now!

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -486,8 +486,8 @@ void Renderer::loadTexture(RawImage* tex)
   checkGLError("Texture not bound.");
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   checkGLError("Pixel storage format not set.");
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   checkGLError("Texture parameters not set.");
